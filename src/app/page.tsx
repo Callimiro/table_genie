cfd6717b0bf3ee109d42bec665f7c84eb78924b1
Rassim Medkour <racimmedkour@gmail.com>
2023-04-11 23:20:59 +0100

    adding images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,11 +89,11 @@ export default function Home() {
               7 photos
             </h1>
             <div className='flex flex-wrap'>
-              <img className='w-56 h-44 mr-1 mb-1 object-cover' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334381.jpg" alt="" />
-              <img className='w-56 h-44 mr-1 mb-1 object-cover' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334382.jpg" alt="" />
-              <img className='w-56 h-44 mr-1 mb-1 object-cover' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334383.jpg" alt="" />
-              <img className='w-56 h-44 mr-1 mb-1 object-cover' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334388.jpg" alt="" />
-              <img className='w-56 h-44 mr-1 mb-1 object-cover' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334385.jpg" alt="" />
+              <img className='w-56 h-44 mr-1 mb-1 object-cover rounded' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334381.jpg" alt="" />
+              <img className='w-56 h-44 mr-1 mb-1 object-cover rounded' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334382.jpg" alt="" />
+              <img className='w-56 h-44 mr-1 mb-1 object-cover rounded' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334383.jpg" alt="" />
+              <img className='w-56 h-44 mr-1 mb-1 object-cover rounded' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334388.jpg" alt="" />
+              <img className='w-56 h-44 mr-1 mb-1 object-cover rounded' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334385.jpg" alt="" />
             </div>
             {/* IMAGES */}
         </div>
