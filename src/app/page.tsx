64043607b4efd01018f890fd1caef298600f34fb
Rassim Medkour <racimmedkour@gmail.com>
2023-04-12 20:16:27 +0100

    added Menu page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,125 +37,32 @@ export default function Home() {
               <a href="" className='mr-7'>Menu</a>
             </nav>
             {/* RESTORANT NAVBAR */}
-            {/* TITLE */}
-            <div className='mt-4 border-b pb-6'>
-              <h1 className='text-6xl font-bold'>
-                101 Holiday Inn
-              </h1>
-            </div>
-            {/* TITLE */}
-            {/* RATING */}
-            <div className='flex items-end'>
-              <div className='mt-2 ratings flex items-center'>
-              <svg className="w-5 h-5 fill-current text-orange-500" viewBox="0 0 24 24">
-                <path
-                  d="M12 16.56l-5.93 3.61 1.79-6.16L3.34 9.79l6.22-.53L12 3.98l2.44 5.28 6.22.53-4.52 3.22 1.79 6.16L12 16.56z">
-                </path>
-              </svg>
-              <svg className="w-5 h-5 fill-current text-orange-500" viewBox="0 0 24 24">
-                <path
-                  d="M12 16.56l-5.93 3.61 1.79-6.16L3.34 9.79l6.22-.53L12 3.98l2.44 5.28 6.22.53-4.52 3.22 1.79 6.16L12 16.56z">
-                </path>
-              </svg>
-              <svg className="w-5 h-5 fill-current text-orange-500" viewBox="0 0 24 24">
-                <path
-                  d="M12 16.56l-5.93 3.61 1.79-6.16L3.34 9.79l6.22-.53L12 3.98l2.44 5.28 6.22.53-4.52 3.22 1.79 6.16L12 16.56z">
-                </path>
-              </svg>
-              <svg className="w-5 h-5 fill-current text-orange-500" viewBox="0 0 24 24">
-                <path
-                  d="M12 16.56l-2.76 1.67.66-2.27L6.68 9.79l2.76-.24L12 4.95l1.56 3.36 2.76.24-2.11 1.5.66 2.27L12 16.56z">
-                </path>
-              </svg>
-              <svg className="w-5 h-5 fill-current text-gray-400" viewBox="0 0 24 24">
-                <path
-                  d="M12 16.56l-5.93 3.61 1.79-6.16L3.34 9.79l6.22-.53L12 3.98l2.44 5.28 6.22.53-4.52 3.22 1.79 6.16L12 16.56z">
-                </path>
-              </svg>
-                <h3 className='text-reg ml-3'>4.5</h3>
-                <h3 className='text-reg ml-3'>600 reviews</h3>
+            {/* MENU */}
+            <main className="bg-white mt-5">
+             <div>
+              <div className="mt-4 pb-1 mb-1">
+                  <h1 className="font-bold text-4xl">
+                    Menu
+                  </h1>
               </div>
-            </div>
-            {/* RATING */}
-        {/* DESCRIPTION */}
-        <div className='mt-4'>
-          <p className='text-lg font-light'>
-            We believe in the commitment to our community and in fostering long-term relationships with locals and tourists. Our menus reflect these connections, featuring local, seasonal produce. Situated in the first line of Altea Beach, with a flowerly terrace, we aim to offer a new experience to our customers
-          </p>
-        </div>
-        {/* DESCRIPTION */}
-            {/* IMAGES */}
-            <h1 className='font-bold text-3lx mt-10 mb-7 border-b pb-5'>
-              7 photos
-            </h1>
-            <div className='flex flex-wrap'>
-              <img className='w-56 h-44 mr-1 mb-1 object-cover rounded' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334381.jpg" alt="" />
-              <img className='w-56 h-44 mr-1 mb-1 object-cover rounded' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334382.jpg" alt="" />
-              <img className='w-56 h-44 mr-1 mb-1 object-cover rounded' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334383.jpg" alt="" />
-              <img className='w-56 h-44 mr-1 mb-1 object-cover rounded' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334388.jpg" alt="" />
-              <img className='w-56 h-44 mr-1 mb-1 object-cover rounded' src="https://resizer.otstatic.com/v2/photos/xlarge/2/50334385.jpg" alt="" />
-            </div>
-            {/* IMAGES */}
-            {/* Reviews */}
-            <div>
-              <h1 className='font-bold text-3xl mt-10 mb-7 border-b pb-5'>
-                What 100 people are saying
-              </h1>
-            </div>
-            {/* Reviews */}
-            {/* Review Card */}
-            <div className="border-b pb-7 mb-7">
-              <div className="flex">
-                <div className="w-1/6 flex flex-col items-center">
-                <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
-                  <h2 className="text-white text-2xl">
-                  RA
-                  </h2>
-                </div>
-                  <p className='text-center'>
-                    Rassim Medkour
+              <div className="flex flex-wrap justify-between">
+                  {/* MENU CARD */}
+                  <div className="border rounded p-3 w-[49%] mb-3">
+                    <h3 className="font-bold text-lg">
+                      Surf and Turf
+                    </h3>
+                    <p className="font-light mt-1 text-sm">
+                      A well done steak with lobster and rice
                     </p>
-                </div>
-                <div className="ml-10 w-5/6">
-                  <div className="flex items-center">
-              <svg className="w-5 h-5 fill-current text-orange-500" viewBox="0 0 24 24">
-                <path
-                  d="M12 16.56l-5.93 3.61 1.79-6.16L3.34 9.79l6.22-.53L12 3.98l2.44 5.28 6.22.53-4.52 3.22 1.79 6.16L12 16.56z">
-                </path>
-              </svg>
-              <svg className="w-5 h-5 fill-current text-orange-500" viewBox="0 0 24 24">
-                <path
-                  d="M12 16.56l-5.93 3.61 1.79-6.16L3.34 9.79l6.22-.53L12 3.98l2.44 5.28 6.22.53-4.52 3.22 1.79 6.16L12 16.56z">
-                </path>
-              </svg>
-              <svg className="w-5 h-5 fill-current text-orange-500" viewBox="0 0 24 24">
-                <path
-                  d="M12 16.56l-5.93 3.61 1.79-6.16L3.34 9.79l6.22-.53L12 3.98l2.44 5.28 6.22.53-4.52 3.22 1.79 6.16L12 16.56z">
-                </path>
-              </svg>
-              <svg className="w-5 h-5 fill-current text-orange-500" viewBox="0 0 24 24">
-                <path
-                  d="M12 16.56l-2.76 1.67.66-2.27L6.68 9.79l2.76-.24L12 4.95l1.56 3.36 2.76.24-2.11 1.5.66 2.27L12 16.56z">
-                </path>
-              </svg>
-              <svg className="w-5 h-5 fill-current text-gray-400" viewBox="0 0 24 24">
-                <path
-                  d="M12 16.56l-5.93 3.61 1.79-6.16L3.34 9.79l6.22-.53L12 3.98l2.44 5.28 6.22.53-4.52 3.22 1.79 6.16L12 16.56z">
-                </path>
-              </svg>
+                    <p className="mt-7">$80</p>
                   </div>
-                  <div className='mt-5'>
-                    <p className='text-lg font-light'>
-                      A lovely place to stop for good and drinks. Quirky surroundings that offer hours of viewing! The food was tasty, service was excellent. Friendly, chatty staff who made you feel very welcome. Definitely worth a visit and I will be back again next time.
-                    </p>
-                  </div>
-                </div>
+                  {/* MENU CARD */}
               </div>
-            </div>
-            {/* Review Card */}
+             </div>
+            </main>
+            {/* MENU */}
         </div>
         </div>
-
       </main>
     </main>
   )
