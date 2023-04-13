a962b25d7baf651f7e86f0820bdf305b0e55eb62
Rassim Medkour <racimmedkour@gmail.com>
2023-04-13 23:49:00 +0100

    search options-search page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,50 +19,48 @@ export default function Home() {
           </div>
         </nav>
         {/* NAVBAR */}
-        {/* HEADER */}
-        <div className=' h-96 overflow-hidden' >
-          <div className='h-full flex justify-center items-center bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2'>
-            <h1 className='text-white text-7xl '>
-              101 Holiday Inn
+        {/* Header */}
+
+        <div className='h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2'>
+          <div className='text-center font-bold mt-10'>
+            <h1 className='text-white text-5xl mb-2 font-sans font-brandon'>Find your table for any occasion
             </h1>
+            {/* SearchBar */}
+            <div className='flex justify-center m-auto py-3 text-left'>
+              <input className='bg-white text-gray-400 border rounded p-2 mr-3 w-80' type="text"
+                placeholder='Location, Restaurant, or Cuisine' />
+              <button className='bg-red-600 px-9 py-2 rounded ml-3 text-white'>Let's go</button>
+            </div>
+            {/* SearchBar */}
           </div>
         </div>
-        {/* HEADER */}
-        {/* DESCRIPTION */}
-        <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-        <div className='bg-white w-[70%] p-3 rounded shadow'>
-            {/* RESTORANT NAVBAR */}
-            <nav className='flex pb-2 border-b text-reg'>
-              <a href="" className='mr-7'>Overview</a>
-              <a href="" className='mr-7'>Menu</a>
-            </nav>
-            {/* RESTORANT NAVBAR */}
-            {/* MENU */}
-            <main className="bg-white mt-5">
-             <div>
-              <div className="mt-4 pb-1 mb-1">
-                  <h1 className="font-bold text-4xl">
-                    Menu
-                  </h1>
+        <div className='flex py-4 m-auto w-2/3 justify-between items-start'>
+          <div className='w-1/5'>
+            <div className="border-b pb-4">
+              <h1 className="mb-2">Region</h1>
+              <p className="font-light text-reg">Toronto</p>
+              <p className="font-light text-reg">Ottawa</p>
+              <p className="font-light text-reg">Montreal</p>
+              <p className="font-light text-reg">Kingston</p>
+              <p className="font-light text-reg">Niagra</p>
+            </div>
+            <div className="border-b pb-4 mt-3">
+              <h1 className="mb-2">Cuisine</h1>
+              <p className="font-light text-reg">Mexican</p>
+              <p className="font-light text-reg">Italian</p>
+              <p className="font-light text-reg">Chinese</p>
+            </div>
+            <div className="mt-3 pb-4">
+              <h1 className='mb-2'>Price</h1>
+              <div className="flex">
+                <button className="border w-full font-light rounded-l pb-2">$</button>
+                <button className="border-r border-t border-b w-full font-light pb-2">$$</button>
+                <button className="border-r border-t border-b w-full font-light rounded-r pb-2">$$$</button>
               </div>
-              <div className="flex flex-wrap justify-between">
-                  {/* MENU CARD */}
-                  <div className="border rounded p-3 w-[49%] mb-3">
-                    <h3 className="font-bold text-lg">
-                      Surf and Turf
-                    </h3>
-                    <p className="font-light mt-1 text-sm">
-                      A well done steak with lobster and rice
-                    </p>
-                    <p className="mt-7">$80</p>
-                  </div>
-                  {/* MENU CARD */}
-              </div>
-             </div>
-            </main>
-            {/* MENU */}
+            </div>
+          </div>
         </div>
-        </div>
+        {/* Header */}
       </main>
     </main>
   )
